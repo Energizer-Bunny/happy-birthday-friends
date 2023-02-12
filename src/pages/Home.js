2118a11db986f0401b19dd5573c24c770d515f3f
Energizer-Bunny <avneeshsinghal2911@gmail.com>
2023-02-12 17:30:05 +0530

    fixed mistake

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,16 @@ import './css/Home.css';
 import sketch from './sketch';
 import p5 from 'p5';
 import data from '../Resources/data.json'
-const TEXT = 'Happy Birthday '+data.name;
+const TEXT = 'Happy Birthday ';
 
-const Home = () => {
+const Home = (props) => {
   const canvasRef = React.useRef(null)
 
   React.useEffect(() => {
     const canvasDivElement = canvasRef.current;
     // NOTE: If you change TEXT value to something else, you'll need to make changes in sketch.js as well to make it work
     // See my comments in sketch.js
-    new p5(sketch(canvasDivElement, TEXT), canvasDivElement);
+    new p5(sketch(canvasDivElement, TEXT+data[props.birthdayGuy]), canvasDivElement);
   })
 
   return (
